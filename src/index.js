9e927493725c1a5ea2681853915470a6153cc3b2
topseySuave <gabrielsuave17@gmail.com>
2018-06-30 21:04:32 +0100

    fix(webpack-plugin-issue): fix webpacl interpolate plugin issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';  // enable in letter version
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import indexRoutes from './routes/index.jsx';
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
+// registerServiceWorker(); // enable in letter version
